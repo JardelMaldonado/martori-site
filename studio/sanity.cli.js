6fcb25fa26c6bb56d3ec9fd2ee5e54280f2f349b
Jardel Maldonado <jardelmalldinado@gmail.com>
2025-07-14 23:25:05 -0300

    Sanity
    Configura hostname do Sanity Studio

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -3,7 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'x4w1q20m',
-    dataset: 'production'
+    dataset: 'production',
   },
+  studio: {
+    hostname: 'martori',
+  },
+  studioHost: 'martori',
   autoUpdates: true,
 })
+
